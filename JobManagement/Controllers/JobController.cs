d0dbca4a9a105cbe45771e1228b2934df880ef75
tomek1kk <tomek1kkgw@gmail.com>
2019-11-20 16:35:18 +0100

    added simple api controller

---
+++ JobManagement/Controllers/JobController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JobManagement.Data;
+using JobManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobManagement.Controllers
+{
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        private ApplicationUoW applicationUoW;
+
+        public JobController(ApplicationUoW applicationUoW)
+        {
+            this.applicationUoW = applicationUoW;
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/GetAll")]
+        public IEnumerable<JobApplication> GetAll()
+        {
+            return applicationUoW.ApplicationsRepository.GetAll();
+        }
+    }
+}
